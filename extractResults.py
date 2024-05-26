c856efa76002b026029e04453d8f3e93976e4033
Atlantik <atlantiklimani1@gmail.com>
2024-05-26 22:18:42 +0200

    Create extractResults.py

---
+++ extractResults.py
@@ -0,0 +1,45 @@
+import os
+import json
+import csv
+import pandas as pd
+
+result = {}
+
+solution_generated_by = 'traffic'
+
+path = f'./output/{solution_generated_by}'
+
+folders = os.listdir(path=path)
+
+for folder in folders:
+    solution_group_path = f'{path}/{folder}'
+    files = os.listdir(path=solution_group_path)
+    json_files = []
+    for file in files:
+        if ".json" in file:
+            json_files.append(file)
+
+    score_field_name = f'{folder} Score'
+    avg_cars_field_name = f'{folder} Average Number of Waiting Cars'
+    completed_path_field_name = f'{folder} Completed Path Number of Cars'
+
+    result[score_field_name] = []
+    result[avg_cars_field_name] = []
+    result[completed_path_field_name] = []
+    
+    for file in json_files:
+        json_file = open(file=f'{solution_group_path}/{file}', mode='r')
+        json_object = json.load(fp=json_file)
+        result[score_field_name].append(json_object['score'])
+        result[avg_cars_field_name].append(json_object['average_waiting_cars'])
+        result[completed_path_field_name].append(json_object['cars_completed'])
+    
+
+df = pd.DataFrame.from_dict(data=result, orient='index')
+df.to_excel('results.xlsx')
+
+# with open('results.csv', 'w') as csvfile:
+#     pd.
+#     writer = csv.writer(csvfile)
+#     for key, value in result.items():
+#         writer.writerow([key, value])
